8855f54aef96f18bc32fd5abef7cea3f124c113c
mahmoud lbabidi <mahmoudlab1982@gmail.com>
2018-07-06 23:53:05 +0200

    new add

--- scrape.js
+++ scrape.js
@@ -1,5 +1,26 @@
-// Your custom scraping script.
 
-// Look at https://github.com/HackYourFutureBelgium/JavaScript2/tree/master/Projects/web-scraper
-// for the version we showed in class.
+document.addEventListener  ("keypress",function() {
 
+    let table = document.querySelector("table.cast_list") ;
+
+  let rows = table.querySelectorAll("tr.even, tr.odd") ;
+    for (let i = 0 ; i <rows.length ; i ++){
+
+        const row = rows[i];
+
+        let actor = row.querySelector("span[itemprop]").innerHTML;+
+        let characterTag  = row.querySelector("td.character");
+        let character;
+        let pos;
+        let a = characterTag.querySelector("a");
+        if (a != null) {
+            character = a.innerHTML;
+        }
+        else {
+         
+          character = characterTag.innerHTML;
+          character=character.slice(-character.length, pos);
+        }
+        console.log("Actor: " + name + " Caharacter: " + character);
+        
+    }
